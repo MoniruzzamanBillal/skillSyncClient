08d083e45771d285454b8e0dac03ba590b5aca78
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2025-07-31 10:22:19 +0600

    auth layout added

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,13 @@
+const LoginPage = () => {
+  return (
+    <div className="mainContainer">
+      <h1>login page </h1>
+      <h1>login page </h1>
+      <h1>login page </h1>
+      <h1>login page </h1>
+      <h1>login page </h1>
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,13 @@
+const RegisterPage = () => {
+  return (
+    <div className="mainContainer">
+      <h1>register page </h1>
+      <h1>register page </h1>
+      <h1>register page </h1>
+      <h1>register page </h1>
+      <h1>register page </h1>
+    </div>
+  );
+};
+
+export default RegisterPage;
