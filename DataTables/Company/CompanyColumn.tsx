5dafd175b95935b3ff6bab5d2aca632aeab13fb5
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2025-08-02 14:20:42 +0600

    update company page added

--- DataTables/Company/CompanyColumn.tsx
+++ DataTables/Company/CompanyColumn.tsx
@@ -1,9 +1,18 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { TCompnay } from "@/utils/global.types";
 import { ColumnDef } from "@tanstack/react-table";
-import { ArrowUpDown } from "lucide-react";
+import { ArrowUpDown, MoreHorizontal } from "lucide-react";
+import Link from "next/link";
 
 export const CompanyColumn: ColumnDef<TCompnay>[] = [
   {
@@ -63,6 +72,41 @@ export const CompanyColumn: ColumnDef<TCompnay>[] = [
           Location
           <ArrowUpDown className="ml-2 h-4 w-4" />
         </Button>
+      );
+    },
+  },
+
+  {
+    id: "actions",
+    header: () => {
+      return <p className=" text-center  ">Action</p>;
+    },
+    cell: ({ row }) => {
+      const rowData = row.original;
+
+      // console.log(rowData);
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem>
+              <Link href={`/dashboard/companies`}>View Details</Link>
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <Link href={`/dashboard/companies/updateCompany/123`}>
+                Update Course
+              </Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       );
     },
   },
